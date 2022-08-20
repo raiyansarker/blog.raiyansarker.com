bb75e63ebd39eb473245a4a4322bd3099aa3a374
Raiyan Sarker <developerraiyan@gmail.com>
2022-08-20 22:30:02 +0600

    Initial App

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="w-screen h-screen grid place-items-center">
+      <h1 className="text-5xl font-bold">Hello World 🎉</h1>
+    </div>
+  );
+}
