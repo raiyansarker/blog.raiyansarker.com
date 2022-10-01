c7382ecc8ab25b10e0720008bccf39146994d83f
Raiyan Sarker <developerraiyan@gmail.com>
2022-10-01 16:36:12 +0600

    Loading animation on newletter button

--- src/components/Footer/Subscribe.tsx
+++ src/components/Footer/Subscribe.tsx
@@ -7,7 +7,12 @@ type formValue = {
 };
 
 const SubscribeForm = () => {
-  const { register, handleSubmit, reset } = useForm<formValue>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<formValue>();
 
   const subscribe: SubmitHandler<formValue> = async ({ email }) => {
     const emailRegex = new RegExp(
@@ -62,7 +67,32 @@ const SubscribeForm = () => {
           placeholder="abc@xyz.com"
           {...register('email', { required: 'Email field is empty' })}
         />
-        <button className="font-semibold text-white rounded text-sm bg-[#292929] w-4/12 lg:w-3/12 h-full focus:outline-none hover:bg-indigo-500 hover:text-white focus:bg-indigo-500 focus:text-white transition-colors">
+        <button
+          disabled={isSubmitting}
+          className="ease-linear flex flex-row items-center justify-center font-semibold disabled:cursor-not-allowed text-white rounded text-sm bg-[#292929] w-4/12 lg:w-3/12 h-full focus:outline-none hover:bg-indigo-500 hover:text-white focus:bg-indigo-500 focus:text-white transition"
+        >
+          {isSubmitting && (
+            <svg
+              className="animate-spin inline-block aspect-square w-4 mr-2 text-white"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx={12}
+                cy={12}
+                r={10}
+                stroke="currentColor"
+                strokeWidth={4}
+              />
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              />
+            </svg>
+          )}
           Subscribe
         </button>
       </form>
