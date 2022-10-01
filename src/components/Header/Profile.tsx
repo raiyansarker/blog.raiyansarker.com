4f557b772d6beea637fe4f998b38f586ddfc78c4
Raiyan Sarker <developerraiyan@gmail.com>
2022-10-01 16:18:58 +0600

    Minor visual changes

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -8,7 +8,7 @@ const Profile = () => {
     <DropdownMenu.Root>
       <DropdownMenu.Trigger className="w-7 border-[1px] border-gray-800 aspect-square rounded-full overflow-hidden focus:ring-1 outline-none focus:ring-offset-2 focus:ring-slate-800">
         <div className="inline-flex overflow-hidden relative justify-center items-center w-full aspect-square bg-[#ECF4FF] rounded-full">
-          <span title="Login" className="font-medium text-sm text-[#4338ca]">
+          <span title="Login" className="font-semibold text-xs text-[#4338ca]">
             RS
           </span>
         </div>
@@ -18,7 +18,7 @@ const Profile = () => {
           sideOffset={5}
           side="bottom"
           align="end"
-          className="border-[1px] border-gray-300 z-10 bg-gray-50 flex flex-col justify-start items-center p-1 rounded shadow-2xl shadow-gray-500 min-w-full"
+          className="border-[1px] border-gray-300 z-10 bg-gray-50/40 backdrop-blur-md flex flex-col justify-start items-center p-1 rounded shadow-2xl shadow-gray-500 min-w-full"
         >
           <Link href="/profile" passHref={true}>
             <DropdownMenu.Item asChild={true}>
