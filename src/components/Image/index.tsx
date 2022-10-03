aea27baa6b489a06607e54de2eee04209d75a147
Raiyan Sarker <developerraiyan@gmail.com>
2022-10-03 20:39:04 +0600

    Updated image src url

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -2,7 +2,7 @@ import NextImage from 'next/image';
 import { ImageProps, ImageLoaderProps } from 'next/image';
 
 const imageLoader = ({ src, width, quality }: ImageLoaderProps): string => {
-  const url = `https://images.raiyansarker.com/${
+  const url = `https://i2.wp.com/${
     src.replace(/^https?:\/\//, '').split(/[?#]/)[0]
   }?w=${width}${typeof quality === 'number' ? `&q=${quality}` : ''}`;
   return url;
